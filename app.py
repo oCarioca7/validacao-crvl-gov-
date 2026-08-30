10841bfbd0cb4a89baa80c8181c2a089a902ab4b
oCarioca7 <maisnovo1771@gmail.com>
2026-08-30 02:07:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ HTML_INTERFACE = """
             const dados = await response.json();
             
             if (response.ok) {
-                // Preenche os campos dinamicamente com os dados retornados
+                            // Preenche os campos dinamicamente com os dados retornados
             if (dados) {
                 for (const [campo, valor] of Object.entries(dados)) {
                     const inputElement = document.getElementById(campo);
@@ -135,6 +135,11 @@ HTML_INTERFACE = """
                         inputElement.value = valor;
                     }
                 }
+            }
+            if (!response.ok && !dados) {
+                alert("Erro ao ler dados: " + (dados.error || "Erro desconhecido"));
+            }
+
             }
             if (!response.ok && !dados) {
                 alert("Erro ao ler dados: " + (dados.error || "Erro desconhecido"));
